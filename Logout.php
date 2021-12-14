856f5a262d51358ea3e2e2eaab13a99321eb77c8
charles Massey <Charles-m@live.ca>
2021-12-13 21:31:48 -0600

    Work on the Login + create the Log out page.

---
+++ Logout.php
@@ -0,0 +1,94 @@
+<?php
+ob_start();
+session_start();
+?>
+
+<html lang="en">
+
+
+<head>
+    <title>Thank you for using the Scheduler</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <style>
+        body {
+            text-align: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ffffff;
+        }
+
+        h2 {
+            text-align: center;
+            color: #000000;
+
+        }
+    </style>
+
+    <!-- Bootstrap CSS -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+
+    <div class="mx-5">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <a class="navbar-brand" href="index.html">Home</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav">
+
+                    <li class="nav-item active">
+
+                        <a class="nav-link" href="studentHome.html">Student</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="schedulerHome.html">Scheduler</a>
+
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </nav>
+        <div class="container">
+            <h2>Thank you for using the Scheduler</h2>
+
+            <div>
+
+                <h2>Make sure to clear your cache nefore close your window.</h2>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap Script -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Javascript -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
