e85372c47d814e2d100f9999ebc99b423bce6882
Pirundy <pierce140he@gmail.com>
2021-12-13 21:32:49 -0600

    mostly done with work on professor homepage

--- professorForm.js
+++ professorForm.js
@@ -1,23 +1,53 @@
 function submitForm() {
     var fCourseID = document.getElementById("courseId");
     var fCourseName = document.getElementById("courseName");
-    var fStarTime = document.getElementById("timeStartId");
+    var fStartTime = document.getElementById("timeStartId");
     var fEndTime = document.getElementById("timeEndId");
     var fSemesters = document.getElementById("semesters")
     var fYears = document.getElementById("years");
-    var f
+    var fPreference = document.getElementById("textSI");
+    var courseForm = document.forms["professorForm"];
 
     var fdays = document.getElementsByClassName("days");
 
     var noDaysSelected = true;
 
-
-
-
-
     for (var i = 0; i < fdays.length; i++) {
         if (fdays[i].checked) {
             noitemsselected = false;
+        }
+    }
+
+    if (!fCourseID.value) {
+        alert("Please enter a valid Course id!")
+    }
+    else if (!fCourseName.value) {
+        alert("Please enter a course name!")
+    }
+    else if (!fStartTime.value) {
+        alert("Please enter a course start time!");
+    }
+    else if (!fEndTime.value) {
+        alert("Please enter a course end time!");
+    }
+    else if (fStartTime.value == fEndTime.value) {
+        alert("Please ensure that your start and end time are not the same!");
+    }
+    else if (!fSemesters.value || fSemesters.value == "select") {
+        alert("Please enter a semester for the course!");
+    }
+    else if (!fYears.value || fYears.value.length < 4) {
+        alert("Please enter a year for the course! Make sure the year is at least 4 characters long!");
+    }
+    else if (!fPreference.value) {
+        alert("Please enter a name for an SI or put none for no one!")
+    }
+    else if (noDaysSelected.value == true) {
+        alert("Please select at least one day for your course!")
+    }
+    else {
+        if (confirm("Are you aready to submit your class?")) {
+            courseForm.submit();
         }
     }
 }
--- professorHome.php
+++ professorHome.php
@@ -1,6 +1,19 @@
 <!doctype html>
 <html lang="en">
 
+<style>
+    .form-control {
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+
+    }
+
+    form {
+        text-align: center;
+    }
+</style>
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -10,13 +23,55 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-    <title>SI Scheduler | Template Page</title>
+    <title>SI Scheduler | Submit Course</title>
 </head>
 
 <body>
+    <?php
+    //checks if all the items are filled out on the previous form or will redirect the user back to the previous page
+    $servername = "143.198.123.91";
+    $database = "AgileExpG4";
+    $username = "user4";
+    $password = "userpwd4";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn) {
+        die("Connection Failed! " . mysqli_connect_error());
+    }
+    if (
+        $_POST["courseId"] && $_POST["courseName"] && $_POST["timeStartId"] && $_POST["timeEndId"] &&
+        $_POST["semesters"] && $_POST["years"] &&  $_POST["textSI"]
+    ) {
+        //variables from the previous page
+        $gCourseId = $_POST["courseId"];
+        $gCourseName = $_POST["courseName"];
+        $gTimeStart = $_POST["timeStartId"];
+        $gTimeEnd = $_POST['timeEndId'];
+        $gSemesters = $_POST["semesters"];
+        $gYears = $_POST["years"];
+        $gTextSI = $_POST["textSI"];
+
+
+        //add the item to the database
+        $sql = "INSERT INTO Courses (courseID, courseDays, courseTimes, courseSemester, courseProfID, courseStuID, courseLength) 
+        VALUES ('courseId', 'courseName', 'timeStartId', 'timeEndId', 'semesters', 'years', 'textSI')";
+        //$sql = "INSERT INTO Games (gameName, gameGenre, gameRating, gameBeaten) VALUES ('$gName', '$gGenre', '$gRating','$gBeaten')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New course added successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        echo "BIG ERROR!!!";
+    }
+    $conn->close();
+    ?>
+
+
     <div class="mx-5">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.html">Home</a>
+            <a class="navbar-brand" href="homePage.html">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -34,7 +89,7 @@
                 </ul>
             </div>
         </nav>
-        <h1>Sample PHP Post</h1>
+        <h1>Course Submission</h1>
     </div>
 
     <!-- Bootstrap Script -->
