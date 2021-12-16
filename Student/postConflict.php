8fd7fbe307c451e842a5a112ca4649d25e46de5d
YoungJoel <joeleyoung01@gmail.com>
2021-12-16 16:04:38 -0600

    Added some php, made it so the index.html reroutes to the homePage.html

--- Student/postConflict.php
+++ Student/postConflict.php
@@ -1,3 +1,27 @@
 <?php
+$servername = "http://143.198.123.91:3306";
+$username = "admin4";
+$password = "pocket4lion";
+$dbname = "AgileExpG4";
 
->
+try {
+    echo "<p> big nice </p>";
+    $newDiverName = $_POST['diverName'];
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("INSERT INTO Diver (FullName) VALUES ( :diverName );");
+    $stmt->bindParam(':diverName', $newDiverName, PDO::PARAM_STR, strlen($newDiverName));
+
+    $stmt->execute();
+
+    echo $conn;
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+header('Location: diver.html');
+
+
+?>
