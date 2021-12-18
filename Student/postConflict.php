43225ef2ab706ec9c3a9f950393936e0ad1ba43a
YoungJoel <joeleyoung01@gmail.com>
2021-12-18 15:15:26 -0600

    attempted to get php file working, successfully connected to db but not yoinking form values yet

--- Student/postConflict.php
+++ Student/postConflict.php
@@ -1,45 +1,79 @@
-<?php
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>FullName</th></tr>";
+<!doctype html>
+<html lang="en">
 
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    function beginChildren() {
-        echo "<tr>";
-    }
+  <title>SI Scheduler | Template Page</title>
+</head>
 
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
+<body>
+  <div class="mx-5">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="index.html">Home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="student/studentHome.html">Student</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="professorHome.html">Professor</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="schedulerHome.html">Scheduler</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <?php
+echo "<h1>BING CHILLING " . "</h1>";
+
+$servername = "143.198.123.91:3306";
+$username = "admin4";
+$password = "pocket4lion";
+$dbname = "AgileExpG4";
+
+foreach($_POST as $key => $val) {
+    echo $val . " ";
 }
 
-$servername = "localhost";
-$username = "diverTestUser";
-$password = "diverTest0!a";
-$dbname = "DiveCompetition";
+$query = "INSERT INTO Conflicts (conflictDays, conflictStartTime, conflictEndTime, conflictType, conflictName, studentID)
+          VALUES ('Mon Tue Wed Thurs Fri', '10:10 AM', '11:05 AM', 'Personal', 'Hockey Practice', '0705516');";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT FullName FROM Diver;");
+    $stmt = $conn->prepare($query);
     $stmt->execute();
 
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
+    echo $result;
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 $conn = null;
-echo "</table>";
 ?>
+  </div>
+
+  <!-- Bootstrap Script -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
